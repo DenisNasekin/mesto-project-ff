381f30e2352fc90b53c4faf03acee14bf61d835c
Denis Nasekin <nasekindo@yandex.ru>
2023-11-09 20:38:32 +0500

    функции готовы

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
+const createCard = function(cardLink, cardTitle) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector('.card__image').src = cardLink;
+  cardElement.querySelector('.card__title').textContent = cardTitle;
+  cardList.append(cardElement); 
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+};
+// @todo: Функция удаления карточки     
+  deleteCard = (event) =>{ 
+  const item = event.target.closest(".card");
+  item.remove();
+  }
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {  
+  createCard(item.link, item.name);
+});
