d0620002d8a7265e641fd234eccd74269c51e98a
Denis Nasekin <nasekindo@yandex.ru>
2023-11-11 02:19:05 +0500

    функции обновлены после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,23 @@
-// @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
-// @todo: DOM узлы
 const cardList = document.querySelector('.places__list');
-// @todo: Функция создания карточки
-const createCard = function(cardLink, cardTitle) {
+
+
+initialCards.forEach(function createCard (item, deleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  cardElement.querySelector('.card__image').src = cardLink;
-  cardElement.querySelector('.card__title').textContent = cardTitle;
-  cardList.append(cardElement); 
+
+  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__image').alt = item.alt;
+  cardElement.querySelector('.card__title').textContent = item.name;
+  
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', deleteCard);
-};
-// @todo: Функция удаления карточки     
-  deleteCard = (event) =>{ 
-  const item = event.target.closest(".card");
-  item.remove();
+ 
+  function deleteCard(event) {
+    const item = event.target.closest('.card');
+    item.remove();
   }
-// @todo: Вывести карточки на страницу
-initialCards.forEach((item) => {  
-  createCard(item.link, item.name);
-});
+  cardList.append(cardElement);  
+  return cardElement;
+})
+
+ 
