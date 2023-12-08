059d05fc27737aa796957b587cc7b0a6b99f5740
Denis Nasekin <nasekindo@yandex.ru>
2023-12-08 08:06:31 +0500

    разкидали функции по файлам

--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,40 @@
-// Функции для работы с карточками проекта Mesto
+function createCard (item, deleteCard, likeIt, openCard) {
+    
+    const cardTemplate = document.querySelector('#card-template').content; 
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    
+    cardElement.querySelector('.card__image').src = item.link; 
+    cardElement.querySelector('.card__image').alt = item.alt; 
+    cardElement.querySelector('.card__title').textContent = item.name; 
+    
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard); 
+    
+    const likeButton = cardElement.querySelector('.card__like-button');
+    likeButton.addEventListener('click', likeIt);
 
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.addEventListener('click', openCard);
+    
+    return cardElement;
+};
 
+function deleteCard(event) { 
+    const item = event.target.closest('.card'); 
+    item.remove();
+}; 
+        
+function likeIt(event) {
+    if (event.target.classList.contains('card__like-button_is-active')) {
+    event.target.classList.remove('card__like-button_is-active');
+    } else {
+    event.target.classList.add('card__like-button_is-active');
+    }
+};
 
-export function createCard (item, deleteCard) { // Создание карточки с колбеком удаления 
-         const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-         cardElement.querySelector('.card__image').src = item.link; 
-         cardElement.querySelector('.card__image').alt = item.alt; 
-         cardElement.querySelector('.card__title').textContent = item.name; 
-         const deleteButton = cardElement.querySelector('.card__delete-button'); 
-         deleteButton.addEventListener('click', deleteCard); 
-         return cardElement;
-        };
+function openCard(itemLink, itemName) {
+    
 
+};
 
-export  function deleteCard(event) { // Удаление карточки 
-          const item = event.target.closest('.card'); 
-          item.remove(); 
-        };  
-
+export {createCard, deleteCard, likeIt, openCard};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import './pages/index.css'; // Импорт стилей
-import {initialCards} from './scripts/cards.js'; // Импорт массива данных
-import {createCard, deleteCard} from './components/card.js'; // Импорт функций работы с карточками
+import './pages/index.css';
+import {initialCards} from './scripts/cards.js';
+import {createCard, deleteCard, likeIt, openCard} from './components/card.js';
+import {openPopap, closePopap} from './components/modal.js';
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
+
+//Объявление попапов
+const popapCard = document.querySelector('.popup_type_image');
+const popapProfile = document.querySelector('.popup_type_edit');
+const popapAddCard = document.querySelector('.popup_type_new-card');
+
+//Объявление кнопок открытия попапов
+const popapCardButton = document.querySelector('.card__image');
+const popapProfileButton = document.querySelector('.profile__edit-button');
+const popapAddCardButton = document.querySelector('.profile__add-button');
+//Объявление кнопки закрытия попапов
+const popapCloseButton = document.querySelectorAll('.popup__close');
+//Объявление кнопки сохранения информации в попапе
+const popapSaveButton = document.querySelector('popup__button');
+
+popapProfileButton.addEventListener('click', openPopap(popapProfile))
+
+popapAddCardButton.addEventListener('click', openPopap(popapAddCard));
 
 
-const cardTemplate = document.querySelector('#card-template').content; // Подключаем блок карт
-const cardList = document.querySelector('.places__list'); // Подключаем список карточек
 
-createCard(cardElement);
+
+
 
 
 
 initialCards.forEach(item => { // Выводим карточки на экран 
-  cardList.append(createCard(item, deleteCard));
+  cardList.append(createCard(item, deleteCard, likeIt, openCard));
 });
 
 
