1ae20ea13709fe3dc39cfd895e1b082921f7c3a3
Denis Nasekin <nasekindo@yandex.ru>
2023-12-30 17:26:29 +0500

    исправлен файл валидации

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,30 +1,30 @@
 //Добавляем класс ошибки
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, formData) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add('form__input_type_error');
+    inputElement.classList.add(formData.inputErrorClass);
+    errorElement.classList.add(formData.errorClass);
     errorElement.textContent = errorMessage;
-    errorElement.classList.add('form__input-error_active');
 };
 
 //Удаляем класс ошибки
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, formData) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.remove('form__input_type_error');
+    inputElement.classList.remove(formData.inputErrorClass);
+    errorElement.classList.remove(formData.errorClass);  
     errorElement.textContent = '';
-    errorElement.classList.remove('form__input-error_active');  
 };
  
 //Добавляем текст ошибки в зависимости от валидности формы
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, formData) => {
     if (inputElement.validity.patternMismatch) {
     inputElement.setCustomValidity(inputElement.dataset.errorMessage);
   } else {
     inputElement.setCustomValidity("");
   }
     if (!inputElement.validity.valid) {
-      showInputError(formElement, inputElement, inputElement.validationMessage);
+      showInputError(formElement, inputElement, inputElement.validationMessage, formData);
     } else {
-      hideInputError(formElement, inputElement);
+      hideInputError(formElement, inputElement, formData);
     }
 };
 
@@ -53,7 +53,7 @@ const setEventListeners = (formElement, formData) => {
     toggleButtonState(inputList, buttonElement, formData)
     inputList.forEach((inputElement) => {
       inputElement.addEventListener('input', function () {
-        checkInputValidity(formElement, inputElement);
+        checkInputValidity(formElement, inputElement, formData);
         toggleButtonState(inputList, buttonElement, formData)
       });
     });
@@ -71,13 +71,13 @@ const enableValidation = (formData) => {
     }
 
 //Функция отчиски ошибок валидации при закрытии формы    
-const clearValidation = (formElement, validationConfig) => {
-    const inputList = Array.from(formElement.querySelectorAll(validationConfig.inputSelector));
-    const buttonElement = formElement.querySelector(validationConfig.submitButtonSelector);
-    buttonElement.classList.add(validationConfig.inactiveButtonClass);
+const clearValidation = (formElement, formData) => {
+    const inputList = Array.from(formElement.querySelectorAll(formData.inputSelector));
+    const buttonElement = formElement.querySelector(formData.submitButtonSelector);
+    buttonElement.classList.add(formData.inactiveButtonClass);
     buttonElement.disabled = true;
     inputList.forEach((inputElement) => {
-      hideInputError(formElement, inputElement, validationConfig.inputErrorClass, validationConfig.errorClass);
+      hideInputError(formElement, inputElement, formData);
       inputElement.setCustomValidity("");
     });
 };
